af242281e0a21da56296536f5b963ebc68188c2c
dikisetiawan1 <dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.com>
2023-05-17 00:11:21 +0700

    add new controller,view and route

---
+++ app/Http/Controllers/AddController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AddController extends Controller
+{
+    public function index(){
+        return view('add');
+    }
+}
---
+++ resources/views/add.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Datas</title>
+</head>
+<body>
+
+<h1 style="text-align: center; color:red">If you want to success, you can hardwork everyday and don't forgot keep learning everytime, everywhere ygy!</h1>
+
+
+
+    
+</body>
+</html>
