250e6554b6a27564955e71aec394af32ee97e87c
dhermawan215 <dhermawan215@gmail.com>
2023-06-30 22:48:04 +0700

    fiitur frontend tracking laundry

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TrackingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('user.index');
+Route::get('/tracking', [TrackingController::class, 'index'])->name('user.track');
 
 
 Route::get('/dashboard', function () {
